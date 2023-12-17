89eac60e6ff1e1945c6929bf6636871f251b2ee0
xahid71 <jahid.info97@gmail.com>
2023-12-17 15:36:22 +0600

    dart : 1656

--- 1656/1656.dart
+++ 1656/1656.dart
@@ -1,22 +1,30 @@
 // Accepted answer, but more time consuming and expensive, have to find another solution
 
+// String truncateSentence(String s, int k) {
+//   String result = "";
+//   int flag = 0;
+
+//   for (int i = 0; i < s.length; i++) {
+//     if (s[i] == " ") {
+//       flag++;
+//     }
+
+//     if (flag < k) {
+//       result += s[i];
+//     } else {
+//       break;
+//     }
+//   }
+
+//   return result.trim(); // Remove trailing spaces
+// }
+
+// ** Final solution **//
+
 String truncateSentence(String s, int k) {
-  String result = "";
-  int flag = 0;
-
-  for (int i = 0; i < s.length; i++) {
-    if (s[i] == " ") {
-      flag++;
-    }
-
-    if (flag < k) {
-      result = result + s[i];
-      print(flag);
-    } else {
-      break;
-    }
-  }
-  return result;
+  List<String> words = s.split(' ');
+  List<String> truncatedWords = words.sublist(0, k);
+  return truncatedWords.join(' ');
 }
 
 main() {
